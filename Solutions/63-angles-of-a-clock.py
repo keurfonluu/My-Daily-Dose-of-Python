e7ace85b3310a38f63ebadeddd3ce477efb4d4af
Keurfon Luu <keurfonluu@outlook.com>
2019-12-27 00:18:31 -0800

    update dip 63

--- Solutions/63-angles-of-a-clock.py
+++ Solutions/63-angles-of-a-clock.py
@@ -3,9 +3,10 @@
 
 #%%
 def calcAngle(h, m):
-    angle_hour = (30 * h) % 360
-    angle_minute = (6 * m - 0.5 * m) % 360
-    return max(angle_hour, angle_minute) - min(angle_hour, angle_minute)
+    angle_hour = (30 * h + 0.5 * m) % 360
+    angle_minute = (6 * m) % 360
+    angle = max(angle_hour, angle_minute) - min(angle_hour, angle_minute)
+    return angle if angle <= 180 else 360 - angle
 
 print(calcAngle(3, 30))
 print(calcAngle(12, 30))
